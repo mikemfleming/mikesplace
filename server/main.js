32a88f45a92596f56b571146c9cbdf835c4ca25f
Michael Fleming <flemingatx@gmail.com>
2016-07-26 09:39:15 -0500

    first commit

---
+++ server/main.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var main = path.join(__dirname, '../public/index.html')
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/Public'));
+
+app.get('/', function(request, response) {
+  response.sendFile(main);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Running on ', app.get('port'));
+});
