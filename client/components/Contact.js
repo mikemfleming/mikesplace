83e8ac41268abcbd968e4772aed0f3815f9fce31
Michael Fleming <flemingatx@gmail.com>
2016-10-09 18:07:22 -0500

    upgrades contact component to class based

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Contact = () => {
-	return (
-	  <div className="contact">
-	    CONTACT
-	    <form action="/sayHello" method="post">
-	        <div>
-	            <label htmlFor="name">Name:</label>
-	            <input type="text" id="name" name="user_name" />
-	        </div>
-	        <div>
-	            <label htmlFor="mail">E-mail:</label>
-	            <input type="email" id="mail" name="user_email" />
-	        </div>
-	        <div>
-	            <label htmlFor="msg">Message:</label>
-	            <textarea id="msg" name="user_message"></textarea>
-	        </div>
-	        
-	        <div className="button">
-	            <button type="submit">Send Your Message</button>
-	        </div>
-	    </form>
-	  </div>
-	)
+class Contact extends Component {
+	constructor(){
+		super()
+	}
+
+	render(){
+		return (
+		  <div className="contact">
+		    CONTACT
+		    <form action="/sayHello" method="post">
+		        <div>
+		            <label htmlFor="name">Name:</label>
+		            <input type="text" id="name" name="user_name" />
+		        </div>
+		        <div>
+		            <label htmlFor="mail">E-mail:</label>
+		            <input type="email" id="mail" name="user_email" />
+		        </div>
+		        <div>
+		            <label htmlFor="msg">Message:</label>
+		            <textarea id="msg" name="user_message"></textarea>
+		        </div>
+		        
+		        <div className="button">
+		            <button type="submit">Send Your Message</button>
+		        </div>
+		    </form>
+		  </div>
+		)
+	}
 }
 
 export default Contact;
