b30ca340fc8a18fd98e76181fdb2b4c611df83f3
Michael Fleming <flemingatx@gmail.com>
2016-10-11 10:42:06 -0500

    moves http req logic into contact component to hide message form after successful send

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import Message from './Message';
 
+import 'whatwg-fetch';
+
 class Contact extends Component {
 	constructor(props){
 		super()
+
+		this.state = {
+			showMessage: true
+		}
 	}
 
 	render(){
 		return (
 		  <div className="contact">
 		    
-		  		<Message handleSubmit={this.props.handleSubmit} />
-
+		    { this.state.showMessage ? 
+		    	<Message handleSubmit={this.handleSubmit.bind(this)} /> :
+		    	<div>nice work!</div> }
+		  	
 	      	<div className="social-media">
 	      		<a href="https://www.linkedin.com/in/mikemfleming">
 			  		<img src="assets/linkedin-icon copy.png" />
@@ -25,6 +33,25 @@ class Contact extends Component {
 	      	</div>
 		  </div>
 		)
+	}
+
+	// TODO: add email verification, protection from deadly hackers, hide form after success 
+	handleSubmit ({ user_name, user_email, user_message }) {
+		// email verification
+
+		fetch('/sayHello', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			// hack protection
+			body: JSON.stringify({ user_name, user_email, user_message})
+		}).then(({status}) => { 
+			console.log('~~~~~~~~~~~status', status)
+			console.log(this.setState({ showMessage: false }))
+		})
+		// on success
 	}
 }
 
--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
-import 'whatwg-fetch';
 
 import Title from './components/Title';
 import Welcome from './components/Welcome';
@@ -11,10 +10,7 @@ import Contact from './components/Contact';
 class App extends Component {
 	constructor(){
 		super()
-
-		this.state = {
-			showMessage: true
-		}
+		
 	}
 
 	render(){
@@ -24,30 +20,10 @@ class App extends Component {
 				<Welcome />
 				<Skills />
 				<Projects />
-				<Contact handleSubmit={this.handleFormSubmit.bind(this)} />
+				<Contact />
 			</div>
 		)
 	}
-
-	// TODO: add email verification, protection from deadly hackers, hide form after success 
-	handleFormSubmit ({ user_name, user_email, user_message }) {
-		// email verification
-
-		fetch('/sayHello', {
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			// hack protection
-			body: JSON.stringify({ user_name, user_email, user_message})
-		}).then(({status}) => { 
-			console.log('~~~~~~~~~~~status', status)
-			console.log(this.state)
-		})
-		// on success
-	}
-
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
