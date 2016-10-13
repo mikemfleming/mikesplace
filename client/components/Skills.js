52c2723fdaf48d51b21bc8392b221dcfd5f41e67
Michael Fleming <flemingatx@gmail.com>
2016-10-13 18:17:14 -0500

    styles skills

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -16,6 +16,11 @@ const Skills = () => {
 	    			After all, that's where the majority of amazing user 
 	    			experiences start.
 	    		</p>
+	    		<p>I collaborate with clients and peers to nurture and 
+	    			transform ideas into well thought out design specs. 
+	    			After all, that's where the majority of amazing user 
+	    			experiences start.
+	    		</p>
 	    	</div>
 	    	<div className="skill-box">
 	    		<i className="fa fa-magic icon" aria-hidden="true"></i>
@@ -24,6 +29,11 @@ const Skills = () => {
 	    			structure, intuitive UI patterns and simple interactions. 
 	    			I'm a minimalist who truly believes that less is more.
 	    		</p>
+	    		<p>I collaborate with clients and peers to nurture and 
+	    			transform ideas into well thought out design specs. 
+	    			After all, that's where the majority of amazing user 
+	    			experiences start.
+	    		</p>
 	    	</div>
 	    	<div className="skill-box">
 	    		<i className="fa fa-code icon" aria-hidden="true"></i>
@@ -31,6 +41,11 @@ const Skills = () => {
 	    		<p>I design in the browser with HTML(5), CSS(3) and a touch of 
 	    			JavaScript. I love coding things from scratch, but I can 
 	    			work with front-end frameworks like Bootstrap too.
+	    		</p>
+	    		<p>I collaborate with clients and peers to nurture and 
+	    			transform ideas into well thought out design specs. 
+	    			After all, that's where the majority of amazing user 
+	    			experiences start.
 	    		</p>
 	    	</div>
 	    </div>
