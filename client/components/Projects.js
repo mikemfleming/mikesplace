254ad64867393c6eef62e0df416c98f5cbdc3384
Michael Fleming <flemingatx@gmail.com>
2016-10-17 10:34:34 -0500

    adds projects header

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Projects = () => {
 		return (
 	  <div className="projects">
-
+	  	<h1>My Projects</h1>
 	  	<div className="overlord">
 	  		<div className="content-text">
 	  			<h1>Overlord</h1>
