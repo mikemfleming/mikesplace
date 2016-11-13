5bd9b3b5cf30c7d433278113f892c8cca05e59e3
Michael Fleming <flemingatx@gmail.com>
2016-11-13 10:30:58 -0600

    bolds text in skills section, removes outline from title

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -5,37 +5,37 @@ const Skills = () => {
 	  <div className="skills">
 	    <div className="content-text">
 	    	<h1>My Skills</h1>
-	    	<h4>I Bring A Lot To The Table In Any Situation</h4>
+	    	<h4><b>Writing clean and reusable code is my passion.</b></h4>
 	    </div>
 	    <div className="skills-gallery">
 	    	<div className="skill-box">
 	    		<i className="fa fa-magic icon" aria-hidden="true"></i>
-	    		<h4>UX/UI</h4>
-	    		<p>
+	    		<h4><b>UX/UI</b></h4>
+	    		<p><b>
 	    			I work together with my team to transorm ideas into well
 	    			thought out designs with an emphasis on user experience.
 	    			I'm all about putting in the work early on to ensure a 
 	    			solid foundation for the duration of the project.
-	    		</p>
+	    		</b></p>
 	    	</div>
 	    	<div className="skill-box">
 	    		<i className="fa fa-code icon" aria-hidden="true"></i>
-	    		<h4>Code</h4>
-	    		<p>
+	    		<h4><b>Code</b></h4>
+	    		<p><b>
 	    			I build things with HTML5, CSS3 and a heavy dose of JavaScript. 
 	    			I love coding things from scratch, but I'm no stranger to inheriting 
 	    			large codebases. 
-	    		</p>
+	    		</b></p>
 	    	</div>
 	    	<div className="skill-box">
 	    		<i className="fa fa-diamond icon" aria-hidden="true"></i>
-	    		<h4>Pedagogy</h4>
-	    		<p>
+	    		<h4><b>Teaching</b></h4>
+	    		<p><b>
 	    			I lead classes on everything from the fundamentals of
 	    			HTML and CSS with elementary school kids to more 
 	    			advanced concepts of JavaScript programming with students 
 	    			at Hack Reactor. 
-	    		</p>
+	    		</b></p>
 	    	</div>
 	    </div>
 	  </div>
