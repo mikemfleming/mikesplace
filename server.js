ee208d24f24396292803117bec4c909e8edbbb16
Michael Fleming <flemingatx@gmail.com>
2017-01-25 17:43:33 -0600

    v2 commit 1

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+var port = process.env.PORT || 8080
+app.listen(port);
+console.log('listening on localhost:' + port);
